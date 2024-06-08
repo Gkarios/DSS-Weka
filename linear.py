ccc8ec17998edc755140b1f97dc1fd8164d643b7
Gkarios <kgareios13@gmail.com>
2024-06-08 10:37:25 +0300

    linear update

--- linear.py
+++ linear.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 def predict_score(age, Mjob, Fjob, reason, failures, higher, freetime, Walc, G1):
     # Encoding categorical variables
-    Mjob_services_health_teacher = 1 if Mjob in ['services', 'health', 'teacher'] else 0
-    Fjob_services_other_health_teacher = 1 if Fjob in ['services', 'other', 'health', 'teacher'] else 0
-    reason_course_home_reputation = 1 if reason in ['course', 'home', 'reputation'] else 0
+    Mjob_services_health_teacher = 1 if Mjob in ['civil services', 'healthcare', 'teacher'] else 0
+    Fjob_services_other_health_teacher = 1 if Fjob in ['civil services', 'other', 'healthcare', 'teacher'] else 0
+    reason_course_home_reputation = 1 if reason in ['school curriculum', 'close from home', 'reputation of school'] else 0
     higher_yes = 1 if higher == 'yes' else 0
 
     # Linear regression function
@@ -27,10 +27,10 @@ def predict_score(age, Mjob, Fjob, reason, failures, higher, freetime, Walc, G1)
 # Streamlit app
 st.title('Predict Score using Linear Regression')
 
-age = st.number_input('Age', min_value=0, max_value=100, value=18)
-Mjob = st.selectbox('Mother\'s Job (Mjob)', ['at_home', 'health', 'services', 'teacher', 'other'])
-Fjob = st.selectbox('Father\'s Job (Fjob)', ['at_home', 'health', 'services', 'teacher', 'other'])
-reason = st.selectbox('Reason for choosing school (reason)', ['home', 'reputation', 'course', 'other'])
+age = st.number_input('Age', min_value=0, max_value=20, value=15)
+Mjob = st.selectbox('Mother\'s Job (Mjob)', ['at home', 'healthcare', 'civil services', 'teacher', 'other'])
+Fjob = st.selectbox('Father\'s Job (Fjob)', ['at home', 'healthcare', 'civil services', 'teacher', 'other'])
+reason = st.selectbox('Reason for choosing school (reason)', ['close from home', 'reputation of school', 'school curriculum', 'other'])
 failures = st.number_input('Number of past class failures (failures)', min_value=0, max_value=10, value=0)
 higher = st.selectbox('Wants to take higher education (higher)', ['yes', 'no'])
 freetime = st.slider('Free time after school (freetime)', min_value=1, max_value=5, value=3)
